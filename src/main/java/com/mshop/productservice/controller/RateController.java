7160efd1a392b7864393013c6df2cbfe62090a2a
giangndj-hlg <giang.ndj.hlg@gmail.com>
2024-05-10 16:31:11 +0700

    fix bug get list rates

--- src/main/java/com/mshop/productservice/controller/RateController.java
+++ src/main/java/com/mshop/productservice/controller/RateController.java
@@ -1,5 +1,7 @@
 package com.mshop.productservice.controller;
 
+import com.mshop.productservice.client.UserClient;
+import com.mshop.productservice.dto.User;
 import com.mshop.productservice.entity.Product;
 import com.mshop.productservice.entity.Rate;
 import com.mshop.productservice.service.ProductService;
@@ -10,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //@CrossOrigin("*")
 @RestController
@@ -21,6 +25,9 @@ public class RateController {
 
     @Autowired
     ProductService productService;
+
+    @Autowired
+    UserClient userClient;
 
     @GetMapping()
     @Transactional(readOnly = true)
@@ -44,6 +51,17 @@ public class RateController {
     @GetMapping("product/{id}")
     public ResponseEntity<List<Rate>> getByProduct(@PathVariable("id") Long id) {
         List<Rate> rates = rateService.findByProduct(id);
+        List<Long> userIds = StreamEx.of(rates).map(Rate::getUserId).toList();
+        List<User> users = userClient.getAllByIds(userIds);
+        Map<Long, User> usersMap = new HashMap<>();
+        for (User user : users) {
+            usersMap.put(user.getUserId(), user);
+        }
+        for (Rate rate : rates) {
+            if (rate.getUserId() != null) {
+                rate.setUser(usersMap.get(rate.getUserId()));
+            }
+        }
         List<Product> products = StreamEx.of(rates).map(Rate::getProduct).toList();
         productService.setProductImage(products);
         return ResponseEntity.ok(rates);
